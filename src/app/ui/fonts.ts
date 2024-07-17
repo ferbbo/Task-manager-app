2e58cc1b8b7f570bed00d5e9fe206409c1f0e8c6
Diego Benitez <diegof.17@hotmail.com>
2024-07-17 14:38:28 -0500

    chore(config): styles

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { DM_Sans } from 'next/font/google'
+
+export const dm_sans = DM_Sans({
+    weight: ['300', '500','700'],
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-dm'
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,44 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: '0.8rem',
+      md: '0.75rem',
+      lg: '0.875rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'white-dark': '#FEF7EE',
+      'beige':'#FEF7EE',
+      'black': '#191B1F',
+      'purple-light': '#DEE9FC',
+      'pink': '#F9E3E2',
+      'orange': '#C18D30',
+      'green': '#4CA154',
+      'green-light': '#E2FBE8',
+      'yellow': '#FDF9C9',
+      'gray-dark': '#2A2D32',
+      'gray': '#3A3E44',
+      'gray-light': '#7E878D',
+      'blue': '#3662E3',
+      'blue-light': '#DEE9FC'
+    },
+    extend: {
+      fontFamily: {
+        dm: ['var(--font-dm-sans)'],
+      },
+    },
   },
   plugins: [],
 }
